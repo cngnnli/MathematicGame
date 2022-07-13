278b4b240c8349224c8f81aa286807fde6692b2b
Can Gonenli <102178923+cngnnli@users.noreply.github.com>
2022-07-13 20:13:42 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Game from "./pages/Game";
+import Board from "./pages/Board";
+import Home from "./pages/Home";
+
+import { ScoreProvider } from "./contexts/Context";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ScoreProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="game" element={<Game />} />
+          <Route path="board" element={<Board />} />
+        </Routes>
+      </BrowserRouter>
+    </ScoreProvider>
   );
 }
 
